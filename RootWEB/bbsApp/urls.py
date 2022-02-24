608582af3894e81b2a3ca20943aa8d1454bd401b
Wonsung2 <q92235@naver.com>
2022-02-24 16:58:43 +0900

    django

--- RootWEB/bbsApp/urls.py
+++ RootWEB/bbsApp/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 from bbsApp import views
 
 urlpatterns =[
-    path('index/', views.index, name='bbs')
+    path('index/', views.index, name='bbs_index'),
+    path('createForm/', views.createForm, name='bbs_createForm'),
+    path('bbs_create/', views.create, name='bbs_create'),
+    path('bbs_read/', views.read, name='bbs_read'),
+    path('bbs_remove/', views.remove, name='bbs_remove'),
+    path('bbs_update/', views.update, name='bbs_update'),
 ]
